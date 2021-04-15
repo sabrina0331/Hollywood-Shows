cc460c49d386346367488d2341e110137a43d376
sabrina0331 <sabrina20520@gmail.com>
2021-04-15 15:24:22 -0700

    first commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '7hn&m+lg#c3t5&!%x181-b0d%4)ox*xd9$t0fc_g^#qjzyvok$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://shows-xyz.herokuapp.com','127.0.0.1']
 
 
 # Application definition
